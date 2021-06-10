834b5f6a63991462522015c1abd4b637899876f6
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-06-09 22:29:35 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// pull current date and time using moment.js
+//add event to schedule
+// be to compare different times in scheduler
+// if time is present is one style
+//if time is past in one style
+//if time is future in one style
+//to save create button to save
+//create save button for each hour
+//listen to all button, but only take input from that certain hour
+// input for each hour
+// time indicator for each hour
+// add event listener to each button 
+// get value of input
+//store value in local storage
+// when page load
+//get all values from local storage
+// set text input to corresponding value
